261771bf9e9012f03952970de1daf75bdc274201
Ayush <114506088+ayushkanyal@users.noreply.github.com>
2023-10-25 08:34:39 +0530

    Update Comment.js

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,28 +1,45 @@
 import style from "./Comment.module.css";
-import reply from "../images/icon-reply.svg";
+import replyIcon from "../../../images/icon-reply.svg";
+import Header from "./Header";
+import VotesCounter from "./VotesCounter";
+import data from "../../../data.json";
+import deleteIcon from "../../../images/icon-delete.svg";
+import editIcon from "../../../images/icon-edit.svg";
 
 export default function Comment(props) {
   return (
     <>
       <div className={style["comment_box"]}>
-        <div className={style["comment_header"]}>
-          <img src="../images/avatars/image-maxblagun.png" alt={props.name}></img>
-          <p className={style["username"]}>{props.name}</p>
-          <div className={style["last_posted"]}>{props.timeAgo}</div>
-        </div>
+        <Header
+          image={props.displayPicture}
+          displayName={props.name}
+          posted={props.timeAgo}
+          newComment={props.currentUser}
+        />
         <p className={style["comment_text"]}>
-          <span className={style["reply_tag"]}>{props.taggedUser}</span> {props.text}
+          <span className={style["reply_tag"]}>{props.taggedUser}</span>{" "}
+          {props.text}
         </p>
         <div className={style["comment_actions"]}>
-          <button className={style["votes_counter"]}>
-            <div className={style["operator"]}>+</div>
-            <div>{props.votes}</div>
-            <div className={`${style["operator"]} ${style["minus"]}`}>-</div>
-          </button>
-          <button className={style["reply_button"]}>
-            <img src={reply} alt="reply action"></img>
-            <p>Reply</p>
-          </button>
+          <VotesCounter initialVotes={props.votes} />
+
+          {props.name === String(data["currentUser"]["username"]) ? (
+            <div className={style["currentUser_actions"]}>
+              <button className={style["delete_button"]}>
+                <img src={deleteIcon} alt="Delete Icon"></img>
+                Delete
+              </button>
+              <button className={style["edit_button"]}>
+                <img src={editIcon} alt="Edit Icon"></img>
+                Edit
+              </button>
+            </div>
+          ) : (
+            <button className={style["reply_button"]}>
+              <img src={replyIcon} alt="reply action" className={style["replyIcon"]}></img>
+              <p>Reply</p>
+            </button>
+          )}
         </div>
       </div>
     </>
