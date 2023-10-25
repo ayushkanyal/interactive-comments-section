c673a4d821f91f82bb2920be1fa19b0b063983bb
Ayush <114506088+ayushkanyal@users.noreply.github.com>
2023-10-25 08:37:21 +0530

    Update CurrentUser.js

--- src/components/CurrentUser.js
+++ src/components/CurrentUser.js
@@ -1,17 +1,35 @@
+import { useState } from "react";
 import style from "./CurrentUser.module.css";
+import image from "../../images/avatars/image-juliusomo.webp";
 
-export default function CurrentUser(){
-    return(
-        <>
-            <div className={style["comment_box"]}>
-                <textarea placeholder="Add a comment..."></textarea>
-                <div className={style["comment_footer"]}>
-                    <img src="https://picsum.photos/200/300" alt="Current User">
+export default function CurrentUser(props) {
+  const [enteredText, setEnteredText] = useState("");
 
-                    </img>
-                    <button>Send</button>
-                </div>
-            </div>
-        </>
-    )
+  function handleChange(event) {
+    setEnteredText(event.target.value);
+    console.log(enteredText);
+  }
+
+  function handleClick(event) {
+    event.preventDefault();
+    props.addComment(enteredText);
+    setEnteredText("");
+  }
+
+  return (
+    <>
+      <form className={style["comment_box"]}>
+        <textarea
+          placeholder="Add a comment..."
+          id="new_comment"
+          value={enteredText}
+          onChange={handleChange}
+        ></textarea>
+        <div className={style["comment_footer"]}>
+          <img src={image} alt={props.CurrentUserName}></img>
+          <button onClick={handleClick}>Send</button>
+        </div>
+      </form>
+    </>
+  );
 }
