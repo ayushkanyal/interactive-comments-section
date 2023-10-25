ae080a6c869cc2f48c09fa2b109a7b4d762fff32
Ayush <114506088+ayushkanyal@users.noreply.github.com>
2023-10-25 08:34:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import Comment from "./components/Comment";
-import RepliesList from "./components/RepliesList";
-import CurrentUser from "./components/CurrentUser";
+import Comment from "./components/UI/Comment/Comment";
+import RepliesList from "./components/UI/RepliesList";
+import CurrentUser from "./components/UI/CurrentUser";
 import data from "./data.json";
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const commentList = data["comments"];
+  const [commentList, setCommentList] = useState(data["comments"]);
 
-  let image = "https://picsum.photos/200/300";
+  function handleComment(text) {
+    setCommentList((prevValue) => [
+      ...prevValue,
+      {
+        id: prevValue.length + 1,
+        content: [text],
+        createdAt: "now",
+        score: 1,
+        user: {
+          image: {
+            png: "./images/avatars/image-juliusomo.png",
+            webp: "./images/avatars/image-juliusomo.webp",
+          },
+          username: "juliusomo",
+        },
+        replies: [],
+      },
+    ]);
+    console.log(commentList[commentList.length - 1]["currentUser"]);
+  }
 
   return (
     <>
@@ -21,12 +41,22 @@ function App() {
               timeAgo={item["createdAt"]}
               text={item["content"]}
               votes={item["score"]}
+              currentUser={
+                String(item["user"]["username"]) ===
+                String(data["currentUser"]["username"])
+                  ? true
+                  : false
+              }
             />
             {<RepliesList key={Math.random()} list={item["replies"]} />}
           </div>
         );
       })}
-      <CurrentUser />
+      <CurrentUser
+        currentUserImage={data["currentUser"]["image"]["png"]}
+        currentUserName={data["currentUser"]["username"]}
+        addComment={handleComment}
+      />
     </>
   );
 }
