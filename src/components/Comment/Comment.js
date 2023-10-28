808b328564b5eda62da8a083abe89944980abee4
Ayush <114506088+ayushkanyal@users.noreply.github.com>
2023-10-28 19:39:45 +0530

    Update Comment.js

--- src/components/Comment/Comment.js
+++ src/components/Comment/Comment.js
@@ -1,10 +1,6 @@
 import style from "./Comment.module.css";
-import replyIcon from "../../../images/icon-reply.svg";
 import Header from "./Header";
-import VotesCounter from "./VotesCounter";
-import data from "../../../data.json";
-import deleteIcon from "../../../images/icon-delete.svg";
-import editIcon from "../../../images/icon-edit.svg";
+import CommentActions from "./CommentActions/CommentActions";
 
 export default function Comment(props) {
   return (
@@ -20,27 +16,7 @@ export default function Comment(props) {
           <span className={style["reply_tag"]}>{props.taggedUser}</span>{" "}
           {props.text}
         </p>
-        <div className={style["comment_actions"]}>
-          <VotesCounter initialVotes={props.votes} />
-
-          {props.name === String(data["currentUser"]["username"]) ? (
-            <div className={style["currentUser_actions"]}>
-              <button className={style["delete_button"]}>
-                <img src={deleteIcon} alt="Delete Icon"></img>
-                Delete
-              </button>
-              <button className={style["edit_button"]}>
-                <img src={editIcon} alt="Edit Icon"></img>
-                Edit
-              </button>
-            </div>
-          ) : (
-            <button className={style["reply_button"]}>
-              <img src={replyIcon} alt="reply action" className={style["replyIcon"]}></img>
-              <p>Reply</p>
-            </button>
-          )}
-        </div>
+        <CommentActions id={(Math.random() * 10)} initialVotes={props.votes} userName={props.name} />
       </div>
     </>
   );
