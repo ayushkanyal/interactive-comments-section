864116236aee320ad988834e252ad543e5e30409
Ayush <114506088+ayushkanyal@users.noreply.github.com>
2023-11-04 22:13:11 +0530

    Update DeleteModal.js
    Added classes and callback function

--- src/components/DeleteModal/DeleteModal.js
+++ src/components/DeleteModal/DeleteModal.js
@@ -1,18 +1,36 @@
 import style from "./DeleteModal.module.css";
 
-export default function DeleteModal() {
+export default function DeleteModal(props) {
+  function handleClick(event) {
+    props.onClick(event.target.value);
+  }
+
   return (
-    <div>
+    <>
       <div className={style["overlay"]}></div>
-      <h1>Delete Comment</h1>
-      <p>
-        Are you sure you want to delete this comment? This will remove the
-        comment and can't be undone.
-      </p>
-      <div>
-        <button>No, cancel</button>
-        <button>Yes, delete</button>
+      <div className={style["actions_box"]}>
+        <h1>Delete Comment</h1>
+        <p>
+          Are you sure you want to delete this comment? This will remove the
+          comment and can't be undone.
+        </p>
+        <div className={style["actions"]}>
+          <button
+            onClick={handleClick}
+            value="cancel"
+            className={style["cancel_button"]}
+          >
+            No, cancel
+          </button>
+          <button
+            onClick={handleClick}
+            value="delete"
+            className={style["delete_button"]}
+          >
+            Yes, delete
+          </button>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
